6f014ed8312005df05537ff34218c01cb394d726
Matt Oatley <Matthew.Oatley225@gmail.com>
2020-07-16 13:33:52 -0500

    created the 3 tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -9,21 +9,44 @@ namespace LoggingKata.Test
         public void ShouldDoSomething()
         {
             // TODO: Complete Something, if anything
+
+            //Arrange
+            TacoParser sut = new TacoParser();
+            //Act
+            var actual = sut.Parse("34,88,Taco Bell Atmore");
+            //Assert
+            Assert.NotNull(actual);
         }
 
         [Theory]
-        [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("34,88,Taco Bell Atmore", 34, 88, "Taco Bell Atmore")]
+        public void ShouldParse(string str, double lat, double log, string expected)
         {
             // TODO: Complete Should Parse
+            //Arrange
+            TacoParser sut = new TacoParser();
+            //Act
+            var actual = sut.Parse(str);
+            //Assert
+            Assert.Equal(expected, actual.Name);
+            Assert.Equal(lat, actual.Location.Latitude);
+            Assert.Equal(log, actual.Location.Longitude);
+
         }
 
         [Theory]
-        [InlineData(null)]
+        //[InlineData(null)]
         [InlineData("")]
+        [InlineData("34,88,Taco Bell Atmore")]
         public void ShouldFailParse(string str)
         {
             // TODO: Complete Should Fail Parse
+            //Arrange
+            TacoParser sut = new TacoParser();
+            //Act
+            var actual = sut.Parse(str);
+            //Assert
+            Assert.Equal(null, actual);
         }
     }
 }
